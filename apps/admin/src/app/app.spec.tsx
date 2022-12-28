4644fe6326e760b18a6746b254ee2ecfe5c72ac8
APIDOPOULOS Konstantinos <kapidopoulos@netcompany-intrasoft.com>
2022-12-28 17:54:31 +0200

    initial commit

--- apps/admin/src/app/app.spec.tsx
+++ apps/admin/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Price/gi)).toBeTruthy();
+    expect(getByText(/Welcome admin/gi)).toBeTruthy();
   });
 });
---
+++ apps/admin/src/app/app.tsx
@@ -0,0 +1,12 @@
+import { Banner } from '@nx-react-starter/common-ui';
+
+export function App() {
+  return (
+    <>
+      <Banner text="Welcome to our admin app." />
+      <div />
+    </>
+  );
+}
+
+export default App;
