dc7783d19b89922a571b3f888139895432ab3fb9
APIDOPOULOS Konstantinos <kapidopoulos@netcompany-intrasoft.com>
2022-12-28 18:22:39 +0200

    lint & format upon commit

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -1,6 +1,8 @@
-import { Banner } from "@nx-react-starter/common-ui";
+import { Banner } from '@nx-react-starter/common-ui';
 
 export function App() {
+  const temp = '';
+
   return (
     <>
       <Banner text="Welcome to our admin app!" />
